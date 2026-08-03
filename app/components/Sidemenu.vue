5d641edcb1535de912d0e51665fb118e974f8342
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2026-08-03 10:01:49 +1000

    Update Sidemenu.vue

--- app/components/Sidemenu.vue
+++ app/components/Sidemenu.vue
@@ -37,7 +37,7 @@
           <!-- Category -->
           <button
             @click="toggle(category.name)"
-            class="flex items-center gap-3 w-full py-2 text-left text-slate-700 hover:text-blue-600"
+            class="flex items-center gap-2 w-full py-2 text-left text-slate-700 hover:text-blue-600"
           >
             <span
               class="w-5 h-5 flex items-center justify-center border rounded text-sm"
