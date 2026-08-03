66c6cca2738bfbacbbd7aaf9f9c87becbf7e5450
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2026-08-03 14:19:22 +1000

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <!-- Main Area -->
   <div class="max-w-7xl mx-auto px-4 py-6 md:flex gap-6">
+    <!-- insert side menu -->
+    <Sidemenu />
 
     <!-- Ad Content -->
     <section class="flex-1">
@@ -24,18 +26,13 @@
         </div>
       </div>
     </section>
-
-    
-    <!-- insert side menu -->
-    <Sidemenu />
+  </div>
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
     <ProductCard
       v-for="product in products"
       :key="product.id"
       :product="product"
     />
-  </div>
-
   </div>
 </template>
 
