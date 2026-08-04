b71d84c0be7925aa5c53c12241b9bb1620fee61e
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2026-08-04 15:57:15 +1000

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss","@nuxtjs/supabase"],
     supabase: {
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_KEY,
