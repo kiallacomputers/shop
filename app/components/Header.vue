fac795121786fd5339b60f4d4301ef7249c5f2a4
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2026-08-05 15:21:22 +1000

    Update Header.vue

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -24,7 +24,7 @@
           >
             Signup/Login
           </NuxtLink>
-
+          <p v-if="user">Welcome {{ user.display_name }}</p>
           <NuxtLink
             v-if="user"
             to="/admin/dashboard"
