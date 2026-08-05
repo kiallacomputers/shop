571c5d17fd605596dc430b53db0d602673dc918c
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2026-08-05 12:33:15 +1000

    Update auth.vue

--- app/layouts/auth.vue
+++ app/layouts/auth.vue
@@ -3,7 +3,7 @@
     <div class="flex justify-center">
       <img class="w-[150px]" src="~/assets/images/logos/kc_logo.png" />
     </div>
-    <div class="flex justify-center text-4xl">
+    <div class="flex justify-center text-4xl mb-6">
       <span class="font-bold text-[#404E71]">Kialla</span
       ><span class="text-[#2CB6D5]">Computers</span>
     </div>
