7f779b1a3104d09d28e93dc19014fdb418d7df1c
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2026-08-06 12:07:59 +1000

    Update [slug].vue

--- app/pages/category/[slug].vue
+++ app/pages/category/[slug].vue
@@ -59,7 +59,7 @@ const { data: products } = await useAsyncData(`products-${slug}`, async () => {
       
           <div
             v-if="products?.length"
-            class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6"
+            class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6"
           >
             <ProductCard
               v-for="product in products"
