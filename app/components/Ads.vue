738b35810f14249c5de140f877e2eb684a3d5770
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2026-08-07 09:42:09 +1000

    Update Ads.vue

--- app/components/Ads.vue
+++ app/components/Ads.vue
@@ -59,4 +59,22 @@ const ads = [
 
 </script>
 
+<style scoped>
+.slide-enter-active,
+.slide-leave-active {
+  transition: transform 0.8s ease;
+}
 
+.slide-enter-from {
+  transform: translateX(100%);
+}
+
+.slide-leave-to {
+  transform: translateX(-100%);
+}
+
+.slide-enter-to,
+.slide-leave-from {
+  transform: translateX(0);
+}
+</style>
