139c02d92bb3c12003884e6c107a27fcb1a6c110
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2026-08-06 10:22:46 +1000

    Update Sidemenu.vue

--- app/components/Sidemenu.vue
+++ app/components/Sidemenu.vue
@@ -125,7 +125,7 @@ const categories = computed(() => {
 
 // Debug output
 watchEffect(() => {
-  console.log("Computed categories:", categories.value);
+  // console.log("Computed categories:", categories.value);
   console.log("Data:", data.value);
 });
 </script>
