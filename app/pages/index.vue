c1fddb8d871e6a5d902543319e9a51782e4da81e
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2026-08-06 12:19:34 +1000

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -30,9 +30,8 @@
 
       <!-- Products -->
       <main class="flex-1">
-        <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 auto-rows-fr">
+        <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
           <ProductCard
-            class="h-full max-h-[500px]"
             v-for="product in featuredProducts"
             :key="product.id"
             :product="product"
