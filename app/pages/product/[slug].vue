2fe7fefc8b3f7c3a9a69f3f092e06fec30a33210
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2026-08-07 11:22:01 +1000

    Update [slug].vue

--- app/pages/product/[slug].vue
+++ app/pages/product/[slug].vue
@@ -29,9 +29,9 @@
               <!-- Refurbished Badge -->
               <span
                 v-if="product.refurbished"
-                class="absolute top-4 -right-8 rotate-45deg bg-red-600 text-white text-xs font-bold text-center w-32 py-1 shadow-lg z-10"
+                class="absolute top-4 -right-8 rotate-45 bg-red-600 text-white text-xs font-bold text-center w-32 py-1 shadow-lg z-10"
               >
-                Featured
+                Refusbished
               </span>
               <img
                 :src="product.images"
