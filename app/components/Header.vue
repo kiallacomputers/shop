f9c3a0274ef0478b40142faca2d1a5bb21c1c3f9
Darren Waack <sales@kiallacomputers.com.au>
2026-08-10 18:13:18 +1000

    .

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -84,6 +84,16 @@
       </div>
     </div>
   </header>
+  <Transition name="toast">
+    <div
+      v-if="cart.notification"
+      class="fixed top-20 right-5 z-[9999] bg-green-600 text-white px-5 py-4 rounded-lg shadow-lg flex items-center gap-3"
+    >
+      <span class="text-xl">✓</span>
+
+      <span>{{ cart.notification }}</span>
+    </div>
+  </Transition>
 </template>
 
 <script setup>
