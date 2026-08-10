9ac83fd2e5eaa520477ae02692ba3dbe64b3d7a3
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2026-08-10 11:56:12 +1000

    Update [slug].vue

--- app/pages/product/[slug].vue
+++ app/pages/product/[slug].vue
@@ -80,7 +80,7 @@
             <!-- Quautity -->
             <div class="flex items-center mt-3">
               <span class="text-[#566C9D]">product Availablity : </span>
-              <span v-if="product.stock > 0" class="ml-2 text-sm text-green-200">
+              <span v-if="product.stock > 0" class="ml-2 text-sm text-teal-800">
                 In Stock
               </span>
               <span v-else class="ml-2 text-sm text-red-800">
