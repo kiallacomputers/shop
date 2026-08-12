3482d3f9bb0c5e4591dcc3d207ad68f8aa277216
Darren Waack <sales@kiallacomputers.com.au>
2026-08-12 18:32:57 +1000

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,5 +22,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     stripeSecretKey: process.env.STRIPE_SECRET_KEY,
     stripeWebhookSecret: process.env.STRIPE_WEBHOOK_SECRET,
+    supabaseSecretKey: process.env.SUPABASE_SECRET_KEY,
+    supabaseUrl: process.env.SUPABASE_URL,
   },
 });
