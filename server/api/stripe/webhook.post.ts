e1e0468c2c8a48b49af1612633f36cfe2b43c40a
Darren Waack <sales@kiallacomputers.com.au>
2026-08-14 09:23:35 +1000

    .

--- server/api/stripe/webhook.post.ts
+++ server/api/stripe/webhook.post.ts
@@ -121,6 +121,10 @@ export default defineEventHandler(async (event) => {
       .eq("id", productId)
       .single();
 
+    if (productErro) {
+      console.error("Supabase stock update error:", updateError);
+    }
+
     console.log("SUPABASE PRODUCT:", productData);
     console.log("SUPABASE ERROR:", productError);
 
