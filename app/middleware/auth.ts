d63191feb847dfd18280cd6ccacdf4c5a62dac7f
Darren Waack <sales@kiallacomputers.com.au>
2026-08-15 16:30:38 +1000

    .

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -2,6 +2,6 @@ export default defineNuxtRouteMiddleware(() => {
   const user = useSupabaseUser();
 
   if (!user.value) {
-    return navigateTo("/auth/signin");
+    return navigateTo("/auth/signin?redirect=/shoppingcart");
   }
 });
