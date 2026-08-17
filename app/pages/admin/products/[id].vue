12ba28ebcd513657bf3ff15766f2988e854a717c
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2026-08-17 13:02:16 +1000

    Create [id].vue

---
+++ app/pages/admin/products/[id].vue
@@ -0,0 +1,8 @@
+<template>
+  <ProductAdminForm mode="edit" :product-id="String(route.params.id)" />
+</template>
+
+<script setup lang="ts">
+definePageMeta({ middleware: "admin" });
+const route = useRoute();
+</script>
