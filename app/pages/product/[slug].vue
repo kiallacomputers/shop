012c0874e9b4288c3c4d53f35525dbdb7fe91bf1
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2026-09-11 20:57:30 +1000

    Add files via upload

--- app/pages/product/[slug].vue
+++ app/pages/product/[slug].vue
@@ -599,7 +599,7 @@
             stroke-linecap="round"
             stroke-linejoin="round"
             stroke-width="2"
-            d="M15 19l-7 7-7-7"
+            d="M9 5l7 7-7 7"
           />
         </svg>
       </button>
