44b97d41c37f9d65a742e09cfbd23a3665a08d39
KiallaComputres <143672068+kiallacomputers@users.noreply.github.com>
2026-09-17 08:27:17 +1000

    Add files via upload

---
+++ server/api/admin/accounting/accounts.get.ts
@@ -0,0 +1,1 @@
+import {getAdminSupabase,requireSuperAdmin} from "~~/server/utils/adminAuth";export default defineEventHandler(async(event)=>{await requireSuperAdmin(event);const{data,error}=await getAdminSupabase().from("accounting_accounts").select("*").order("code");if(error)throw createError({statusCode:500,statusMessage:error.message});return data||[]});
